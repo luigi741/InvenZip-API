96a0766a107a17c793aab771361f3a30b6679cd6
Luis Castro <luigi_789@hotmail.com>
2019-04-09 02:28:27 -0400

    WIP | Writing to database

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -1,9 +1,9 @@
-//========================================================
+//==============================================================================
 // routes.js
 // This holds the different routes for the Invenzip app
 // and all of the CRUD operations
 // By: Luis Castro
-// =======================================================
+//==============================================================================
 
 var express     = require('express');
 var bodyParser  = require('body-parser');
@@ -17,6 +17,20 @@ module.exports = function(app, db) {
     app.post('/test', function(req, res) {
         res.send('Hello, response from /test route!');
         console.log(req.body);
+
+        var testObject = {
+            name: 'lima',
+            location: 'charlie'
+        };
+
+        db.collection('scanData').insert(testObject, function(err, result) {
+            if (err) {
+                res.send( {'error': 'An error has occured' });
+            }
+            else {
+                res.send(result.ops[0]);
+            }
+        });
     });
     app.post('/upcScan', function(req, res) {
         res.send('Camera scanning function request received.');
--- server.js
+++ server.js
@@ -1,23 +1,31 @@
-//========================================================================
+//==============================================================================
 // server.js
 // Main server side code for InvenZip
 // By: Luis Castro
-// =======================================================================
+// =============================================================================
 
 var express         = require('express');
 var MongoClient     = require('mongodb').MongoClient;
 var bodyParser      = require('body-parser');
 var cors            = require('cors');
+var db              = require('./config/db');
 var app             = express();
+
+const port = 3000;
+require('./app/routes/routes')(app, {});
 
 app.use(
     cors({ origin: true }),
     bodyParser.urlencoded({ extended: true }),
     bodyParser.json()
 );
-require('./app/routes/routes')(app, {});
-const port = 3000;
 
-app.listen(port, function() {
-    console.log('API is live on ' + port);
+MongoClient.connect(db.url, function(err, database) {
+    if (err) {
+        return console.log(err);
+    }
+
+    app.listen(port, function() {
+        console.log('API is live on ' + port);
+    });
 });
